c4a80932f546774de49b67ec079aabf9506f1c11
Mara Timberlake and Steve Zaske <steve@>
2017-06-05 14:24:27 -0700

    initial array examples

---
+++ ArrayMapping.js
@@ -0,0 +1,16 @@
+//make an Array
+var originalNumbers = [1,2,3,23];
+//create another array based on the first Array
+var newNumbers = originalNumbers.map(function(number){
+  return number+1;
+});
+// for each array item our new array with have +1
+
+console.log(newNumbers);
+
+
+var words = ["jane", "clare", "anna"];
+var newWords = words.map(function(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+});
+console.log(newWords);
